97070cdc691c362fdd3c355913042060f6512f9b
semasping <semasping@gmail.com>
2018-07-13 13:15:16 +0200

    [add] setting for cron on production server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,19 +13,28 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        if (getenv('APP_ENV') == 'production') {
+            $schedule->command('bch:get-block')->everyMinute()->withoutOverlapping();
+            //$schedule->command('steemit:getblock')->everyMinute()->withoutOverlapping()   ;
+        }
+        if (getenv('APP_ENV') == 'production') {
+            $schedule->command('GolosVoterBot:checkPosts')->everyMinute()->withoutOverlapping();
+            //$schedule->command('steemit:getblock')->everyMinute()->withoutOverlapping()   ;
+
+        }
     }
 
     /**
@@ -35,7 +44,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

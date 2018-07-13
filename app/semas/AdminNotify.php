33bdc1dddb2a2b399e07c20fc998992026d189ff
semasping <semasping@gmail.com>
2018-07-13 13:08:15 +0200

    [add] GolosApi and SteemitApi for working with blockchains

---
+++ app/semas/AdminNotify.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: semasping (semasping@gmail.com)
+ * Date: 16.08.2017
+ * Time: 13:10
+ */
+
+namespace App\semas;
+
+
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class AdminNotify
+{
+
+    public static function send($text, $full = false)
+    {
+        try {
+            if ($full) {
+                $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 1);
+                $text = 'Full ' . print_r($trace, true) . ' ' . $text;
+            }
+            $text = '#' . env('APP_ENV') . ' : ' . env('APP_NAME') . ' ' . $text;
+            $laravel = app();
+            $version = $laravel::VERSION;
+            if (str_contains($version,  '5.6.')) {
+                Log::channel('slack')->info($text);
+            } else {
+                Telegram::setAccessToken(getenv('TELEGRAM_BOT_TOKEN'))->sendMessage([
+                    'chat_id' => '147893636',
+                    'text' => $text,
+                ]);
+            }
+        } catch (\Exception $e) {
+            //echo $e->getMessage();
+        }
+    }
+}
---
+++ app/semas/GolosApiWsConnector.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: semasping (semasping@gmail.com)
+ * Date: 14.11.2017
+ * Time: 10:39
+ */
+
+namespace App\semas;
+
+
+use GrapheneNodeClient\Connectors\WebSocket\WSConnectorAbstract;
+use WebSocket\ConnectionException;
+
+class GolosApiWsConnector extends WSConnectorAbstract
+{
+    /**
+     * @var string
+     */
+    protected $platform = self::PLATFORM_GOLOS;
+
+    /**
+     * wss or ws server
+     *
+     * @var string
+     */
+    protected static $nodeURL = [
+        //'ws://80.241.216.146:8090',// не работаю никакие варианты
+
+        'wss://api.golos.cf',
+        'wss://ws.golos.blog',
+        'wss://17.golos.cf',
+
+        //'wss://ws.golos.io',
+        //'wss://ws17.golos.io',
+
+    ];
+    //protected static $nodeURL = ['wss://ws.golos.io','wss://api.golos.cf'];
+    //protected $nodeURL = 'wss://api.golos.cf';
+    //protected static $nodeURL = 'ws://80.241.216.146:8090';// не работаю никакие варианты
+
+
+
+    public function destroyConnection() {
+        //self::$connection = null;
+    }
+
+
+}
